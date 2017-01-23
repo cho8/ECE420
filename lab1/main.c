993da238370486cb34ae259303b18f7f1ec29b40
cho8 <cho8@ualberta.ca>
2017-01-23 13:29:04 -0700

    matrix multiply working, serialtester is segfaulting

--- lab1/main.c
+++ lab1/main.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <math.h>
 
-#include "lab1_IO.h"
+#include "sdk/lab1_IO.h"
+#include "sdk/timer.h"
 
 /*
 	Usage ./main thread_count
@@ -17,51 +19,113 @@ int thread_count;
 int **A;	// int array pointer to input matrix A
 int **B;	// int array pointer to input matrix B
 int **C;	// int array pointer to result matrix
-double time;	// time measurement
 
 /* Serial function */
 
 /* Parallel function */
-void *matrix_mul(void* rank);
+void* matrix_mul(void* rank);
+void print_matrix(int** m);
 
 /*** Main ***/
 int main(int argc, char* argv[]) {
 
 	// Local variables
-	long       thread;
+	long thread;
 	pthread_t* thread_handles;
+	double start_time;
+	double end_time;
 
 
-	// TODO get dimensions (from either command line or otherwise)
+	// get dimensions (from either command line or otherwise)
 	if (argc !=2 ) {
 		printf("Usage: ./%s <threads>\n", argv[0]);
+		return 0;
 	}
+
 	thread_count = atoi(argv[1]);
+	thread_handles = malloc(thread_count*sizeof(pthread_t));
 
 
-	// TODO load in matrices using lab1_IO.c
-	// memory is allocated automatically
+	// load in matrices using lab1_IO.c
+	// allocate mem for matrix C
 	Lab1_loadinput(&A, &B, &n);
 
-	// TODO matrix multiplication serial (i.e. 1 thread)
+	C = (int**) malloc(n*sizeof(int**));
+	for (int i=0; i<n; i++) {
+		C[i] = (int*) malloc (n*sizeof(int*));
+	}
 
 
-	// TODO matrix multiplication parallel (and time)
+	// matrix multiplication parallel (and time)
 	// create threads and perform matrix_mul
+	GET_TIME(start_time);
+	for (thread = 0; thread < thread_count; thread++)
+	   pthread_create(&thread_handles[thread], NULL,
+		  matrix_mul, (void*) thread);
 	// join threads
+	for (thread = 0; thread < thread_count; thread++)
+       pthread_join(thread_handles[thread], NULL);
 
-	// TODO save matrix using lab1_IO.c
-	Lab1_saveoutput(C, n, time); // NOTE: output matrix arg might be weird here
+	GET_TIME(end_time);
 
-	// TODO free matrix mallocs
+	// save matrix using lab1_IO.c
+	Lab1_saveoutput(C, &n, end_time-start_time); // NOTE: output matrix arg might be weird here
 
+	print_matrix(C);
 
+	// free matrix mallocs
+	free(A);
+	free(B);
+	free(C);
 
 	return 0;
 }
 
 /*
 	Parallel matrix multiplication
-	Multiple nxn matrix A by nxn matrix B
-	Args: rank
+	Multiple nxn matrix A by nxn matrix B by allowing a thread to calculate a
+	block of elements in the result matrix.
+	Block division scheme described in Lab1 manual.
+	Variables used also correspond to the ones defined in the manual.
+	Args: rank -- thread rank
 */
+void* matrix_mul(void* rank) {
+	long k = (long)rank;		// my_rank of thread
+	int p = thread_count;
+
+	// Calculate xy of block
+	int x = k/sqrt(p);
+	int y = (int)k % (int)sqrt(p);
+	int i,j,m;
+
+	// Calculate upper and lower bounds for x and y of block
+	int lower_x = (n/sqrt(p))*x;
+	int upper_x = (n/sqrt(p))*(x+1) - 1;
+	int lower_y = (n/sqrt(p))*y;
+	int upper_y = (n/sqrt(p))*(y+1) - 1;
+
+	// for each element in the block
+	for (i=lower_x; i<=upper_x;i++) {
+		for(j=lower_y; j<=upper_y;j++) {
+
+			// do colxrow multiplication for the element
+			for(m=0; m<n; m++) {
+				C[i][j] += A[m][j] * B[i][m];
+			}
+		}
+	}
+	return NULL;
+}
+
+/*
+	Print matrix for funsies
+*/
+void print_matrix(int** C) {
+	int i,j;
+	for (i=0; i<n; i++) {
+		printf("\n");
+		for(j=0; j<n;j++) {
+			printf("%d ", C[i][j]);
+		}
+	}
+}
